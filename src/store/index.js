02f95207729aa08874882c680bec7ce59b98605a
Ihsan <ihsanmchink@gmail.com>
2018-03-05 21:59:39 +0700

    Setup Firebase Config

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,16 @@
+import firebase from 'firebase'
+
 export const strict = false
+
+let config = {
+  apiKey: 'AIzaSyCN39p7EvVlvChIV9lhNRpXNqQ5PLKsBkw',
+  authDomain: 'dotapp-69dcf.firebaseapp.com',
+  databaseURL: 'https://dotapp-69dcf.firebaseio.com',
+  projectId: 'dotapp-69dcf',
+  storageBucket: 'dotapp-69dcf.appspot.com',
+  messagingSenderId: '5998765842'
+}
+firebase.initializeApp(config)
 
 export const state = () => ({
   loading: false,
@@ -9,4 +21,7 @@ export const state = () => ({
 })
 
 export const mutations = {
+}
+
+export const actions = {
 }
